ff8463a4e728db5c3663f32e1066c34b794ba499
Carlos Bautista <carlosbm@gmail.com>
2019-06-26 21:00:50 +0200

    First version

---
+++ json2ngsi.js
@@ -0,0 +1,26 @@
+var argv = require('optimist').argv;
+var fs   = require('fs');
+var ngsi = require('ngsi-parser');
+
+var index = 0;
+
+function main () {
+    var files = argv._;
+    var readOption = { encoding: 'utf8' };
+
+    var readFunc;
+    readFunc = function (err, data) {
+        console.log(JSON.stringify(ngsi.parseEntity(JSON.parse(data))));
+	
+
+       /* var nextFile = files.shift();
+        if (nextFile !== undefined) {
+            fs.readFile(nextFile, readOption, readFunc);
+        }
+*/
+    };
+
+    fs.readFile(files[0], readOption, readFunc);
+}
+
+main();
